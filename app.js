511d7f6a7ae9525aa41eb3ea9ada465c6a58089b
Aman kumar singh <kumarsinghaman3500@gmail.com>
2022-12-19 20:34:40 +0530

    adding encryption

---
+++ app.js
@@ -0,0 +1,71 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const ejs = require("ejs");
+
+const mongoose = require("mongoose");
+const encrypt = require('mongoose-encryption');
+
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb://localhost:27017/userDB", { useNewUrlParser: true });
+
+const userSchema = new mongoose.Schema({
+  email: String,
+  password: String,
+});
+
+
+const secret = "Thisisoutlittlesecret";
+userSchema.plugin(encrypt, {secret : secret, encryptedFields : ['password']});
+
+const User = new mongoose.model("User", userSchema);
+
+app.get("/", (req, res) => {
+  res.render("home");
+});
+
+app.get("/login", (req, res) => {
+  res.render("login");
+});
+
+app.post("/login", (req, res) => {
+  const username = req.body.username;
+  const password = req.body.password;
+  User.findOne({ email: username }, (err, result) => {
+    if (err) {
+      res.send(err);
+    } else {
+      if (result) {
+        if (result.password === password) {
+          res.render("secrets");
+        } else {
+          res.send(err);
+        }
+      }
+    }
+  });
+});
+
+app.get("/register", (req, res) => {
+  res.render("register");
+});
+app.post("/register", (req, res) => {
+  const newUser = new User({
+    email: req.body.username,
+    password: req.body.password,
+  });
+  newUser.save((err) => {
+    if (err) {
+      console.log(err);
+    } else {
+      res.render("secrets");
+    }
+  });
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port 3000");
+});
