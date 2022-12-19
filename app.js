fbb112a769bb40dd5430672ce0684e47f3ce9a2f
Aman kumar singh <kumarsinghaman3500@gmail.com>
2022-12-19 20:59:27 +0530

    adding encryption

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+require('dotenv').config();
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
@@ -16,10 +17,10 @@ const userSchema = new mongoose.Schema({
   email: String,
   password: String,
 });
+// console.log(process.env.API_KEY);
 
 
-const secret = "Thisisoutlittlesecret";
-userSchema.plugin(encrypt, {secret : secret, encryptedFields : ['password']});
+userSchema.plugin(encrypt, {secret : process.env.SECRET, encryptedFields : ['password']});
 
 const User = new mongoose.model("User", userSchema);
 
